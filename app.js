d53124724204964dc33adc6b37c4a2d53412997a
Jhubkonto <julian.lacis@gmail.com>
2021-02-21 22:03:37 +0100

    Readd headers

--- app.js
+++ app.js
@@ -18,13 +18,12 @@ app.use(bodyParser.json());
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
-  // res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    // 'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
   next();
 });
